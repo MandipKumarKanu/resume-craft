9f0b5dda9b82b048977454409956132f1129f490
Mandip Kumar Kanu <mandyshah2003@gmail.com>
2025-06-25 00:23:48 +0545

    added cors url

--- backend/index.js
+++ backend/index.js
@@ -13,9 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-    ],
+    origin: ["http://localhost:5173", "https://resume-craft-474l.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
